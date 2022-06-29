2458ecc3b6e42acff4bad469fdba5722d12d37eb
matmagamb <ceifa13@outlook.com>
2022-06-29 15:45:45 -0300

    aula componente Sidebar finalizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import stylesSidebar from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
     return (
@@ -8,12 +9,19 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             <div className={stylesSidebar.profile}>
+                <img
+                    className={stylesSidebar.avatar}
+                    src="https://github.com/matmagamb.png" />
+
                 <strong>Matheus Ambrosio</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    editar seu perfil
+                </a>
             </footer>
 
         </aside>
