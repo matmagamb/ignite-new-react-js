094a04bc2a5f7836d2e1de39ed2bcc8ced447a35
matmagamb <ceifa13@outlook.com>
2022-07-04 10:34:34 -0300

    aula componente component post

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
-import styleAppmodule from './App.module.css'
-import './global.css';
+import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
+
+import './global.css';
+import styleAppmodule from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
       <div className={styleAppmodule.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-
-          <Post
-            author="Matheuss Ambrosio"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maxime, blanditiis minima quae eius necessitatibus praesentium est reiciendis accusantium voluptate enim officiis cupiditate neque saepe, laudantium rem. Excepturi, fugit aut?"
-          />
-          <Post
-            author="Matheuss Magalhães"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maxime, blanditiis minima quae eius necessitatibus praesentium est reiciendis accusantium voluptate enim officiis cupiditate neque saepe, laudantium rem. Excepturi, fugit aut?"
-          />
+          <Post />
 
         </main>
       </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.Post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/matmagamb.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Matheus Ambrosio</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='04 de julho as 10:21' dateTime='2022-07-04 10:21:30'>publicado hà 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉 <a href="#"> jane.design/doctorcare </a></p>
+
+                <p> <a href="#"> #novoprojeto #nlw #rocketseat  </a></p>
+
+            </div>
+        </article>
+    )
+
+}
