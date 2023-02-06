c9edf408d2b0881977a626d9c7472075adf375e8
Matheus Ambrosio <ceifa13@outlook.com>
2023-02-06 17:36:58 -0300

    component Avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import HeaderStyle from './Header.module.css';
-import igniteLogo from '../assets/ignite-logo.svg'
+import styles from "./Header.module.css";
+import igniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
-    return (
-        <header className={HeaderStyle.header}>
-            <img src={igniteLogo} alt="logotipo do ignite" />
-        </header>
-    )
-
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="logotipo do ignite" />
+    </header>
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,27 @@
 import stylesSidebar from "./Sidebar.module.css";
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
-    return (
-        <aside className={stylesSidebar.sidebar}>
-            <img
-                className={stylesSidebar.cover}
-                src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-            />
-            <div className={stylesSidebar.profile}>
-                <img
-                    className={stylesSidebar.avatar}
-                    src="https://github.com/matmagamb.png" />
+  return (
+    <aside className={stylesSidebar.sidebar}>
+      <img
+        className={stylesSidebar.cover}
+        src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={stylesSidebar.profile}>
+        <Avatar src="https://github.com/matmagamb.png" />
 
-                <strong>Matheus Ambrosio</strong>
-                <span>Web Developer</span>
-            </div>
+        <strong>Matheus Ambrosio</strong>
+        <span>Web Developer</span>
+      </div>
 
-            <footer>
-                <a href="#">
-                    <PencilLine size={20} />
-                    editar seu perfil
-                </a>
-            </footer>
-
-        </aside>
-    )
-
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
